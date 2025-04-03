4e44379a7bf26a149f01da2687b6924fb39ad99a
Thomas Milton <perso.milton@gmail.com>
2025-04-03 09:14:23 +0200

    add Telemetry.

--- ColorsApp/MauiProgram.cs
+++ ColorsApp/MauiProgram.cs
@@ -14,7 +14,7 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
-
+        
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
@@ -22,3 +22,34 @@ public static class MauiProgram
         return builder.Build();
     }
 }
+
+#if macOS
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+ 
+#endif
+
+#if macOS
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService("Colors-App"))
+            .WithTracing(tracing => tracing
+                .AddHttpClientInstrumentation()
+                .AddAspNetCoreInstrumentation())
+            .WithMetrics(metrics => metrics
+                .AddAspNetCoreInstrumentation()
+                .AddRuntimeInstrumentation())
+            .UseOtlpExporter(
+                OpenTelemetry.Exporter.OtlpExportProtocol.Grpc,
+                new Uri("http://127.0.0.1:4317")
+            );
+ 
+        builder.Logging.AddOpenTelemetry(options =>
+        {
+            options.IncludeScopes = true;
+            options.IncludeFormattedMessage = true;
+        });
+ 
+#endif
